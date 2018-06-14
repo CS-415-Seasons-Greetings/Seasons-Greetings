5f4abd20fe9cf0898b6239cb9984e2bdae6c76ff
Nick Daddona <nickd1957@comcast.net>
2018-06-13 21:21:04 -0400

    Added initial player movement
    The player can now move left or right. The player sprite will now change
    direction to match the direction its moving. Fixed disjointed player
    hitboxes and resized sprite to match current hitboxes. Fixed main camera
    so it will follow the player an an appropriate size and won't show lower
    boundries.

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    [HideInInspector] public bool facingRight = true;
+    [HideInInspector] public bool jump = false;
+
+    public float moveForce = 150f;
+    public float maxSpeed = 2f;
+    public float jumpForce = 1000f;
+    public Transform groundCheck;
+
+    private bool grounded = false;
+    private Animator anim;
+    private Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        if (Input.GetButtonDown("Jump") && grounded == true) // jumping is currently not working
+        {
+            jump = true; // jump if the jump button is pressed and the character isn't grounded
+        }
+    }
+
+    /**
+     * Controls Physics Updates
+     * 
+     */
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", Mathf.Abs(h));
+
+        if (h * rb2d.velocity.x < maxSpeed)
+        { // ensure the speed is based on the direction
+            rb2d.AddForce(Vector2.right * h * moveForce);
+        }
+
+        if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
+        { // check to see if character hasn't broken max speed
+            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y); // preserve direction with Mathf.Sign()
+        }
+
+        if (h > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (h < 0 && facingRight)
+        {
+            Flip();
+        }
+
+        if (jump)
+        {
+            anim.SetTrigger("Jump");
+            rb2d.AddForce(new Vector2(0f, jumpForce)); // add jump force to the sprite
+            jump = false;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
