c0fd7886a372d9e1f523fd19739d6236979d1ec0
Nick Daddona <nickd1957@comcast.net>
2018-06-20 17:38:59 -0400

    Added Directional Knockback to player
    The player will now be knocked back in the opposite direction when it is
    hit by an enemy. The enemy will currently still move towards the player,
    this will have to be changed in the future.

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -15,7 +15,6 @@ public class PlayerHealth : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
         if (gameObject.transform.position.y < -4 || health < 1)
         {
             Die();
@@ -25,30 +24,33 @@ public class PlayerHealth : MonoBehaviour {
     void Die()
     {
         hasDied = true;
-        SceneManager.LoadScene("Level design");
-        
+        //SceneManager.LoadScene("Level design");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // reloads the current scene when the player dies
     }
 
     public void TakeDamage(int amount)
     {
         health -= amount;
-
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Enemy")
+        if (collision.gameObject.tag == "Enemy" && GetComponent<PlayerControl>().grounded)
         {
-            TakeDamage(1);
-            //collision.gameObject.GetComponent<EnemyHealth>().TakeDamage(1);
-            //Debug.Log(GetComponent<Enemy>().knockedBack);
-           // if (GetComponent<PlayerControl>().grounded)
-            //{
-                Debug.Log("Knockback");
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(500, 50));
-            
-            //}
+            TakeDamage(1); // deal 1 damage to the player
+            //collision.gameObject.GetComponent<EnemyHealth>().TakeDamage(1); // possibly deal contact damage to enemies
+            //Debug.Log(GetComponent<Enemy>().knockedBack); // causes some errors
+            // if (GetComponent<PlayerControl>().grounded
+            Debug.Log("Knockback");
+            Vector2 enemypos = collision.gameObject.GetComponent<Transform>().position; // obtain the enemy position
+            if (enemypos.x < GetComponent<Transform>().position.x)
+            { // knockback to the right
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(500, 50)); // add force to the player when hit
+            }
+            else
+            { // knockback to the left
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(-500, 50)); // add force to the player when hit
+            }
         }
     }
-
 }
