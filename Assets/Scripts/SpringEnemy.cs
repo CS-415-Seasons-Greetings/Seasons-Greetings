65374e68f64f1f1d3c5ae45f9040b9f0a7e6d2df
Josh <Bustados34@gmail.com>
2018-06-27 00:55:36 -0400

    Add spring enemy tracking ability and prefab
    Its ugly, but spring enemy does track player, also created a good prefab
    to work with.

--- Assets/Scripts/SpringEnemy.cs
+++ Assets/Scripts/SpringEnemy.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpringEnemy : MonoBehaviour {
+public class SpringEnemy : MonoBehaviour
+{
 
     public GameObject target; //the enemy's target
-    public float moveSpeed = 5; //move speed
+    public float moveSpeed = 0.01f; //move speed
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player");
@@ -13,11 +14,13 @@ public class SpringEnemy : MonoBehaviour {
     void Update()
     {
 
-        Vector3 targetDir = target.transform.position - transform.position;
-        float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg - 90f;
-        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 180);
-        transform.Translate(Vector3.up * Time.deltaTime * moveSpeed);
-    }
+        /* Vector3 targetDir = target.transform.position - transform.position;
+         float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg - 90f;
+         Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+         transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 180);
+         transform.Translate(Vector3.up * Time.deltaTime * moveSpeed);
+         */
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
 
+    }
 }
