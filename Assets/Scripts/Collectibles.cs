719fa8f6265a23469ce518f17824e0cc6ad2df47
Josh <Bustados34@gmail.com>
2018-07-02 21:01:14 -0400

    Cleaned up folders and added collectibles to all level
    Renamed, and moved all prefabs and spites to the level that they correspond to. Added in sprites for the
    collectibles and started the script for keeping score. Currently they just delete on contact. Score is incremented,
    but not shown to the player. All levels have their own collectible and they have be added to the scenes. Tutorial
    level shows what to collect,  Spring has different color flowers that do nothing, fall will have fake pumpkins that
    do damage.

---
+++ Assets/Scripts/Collectibles.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectibles : MonoBehaviour {
+
+    static int Score;
+
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Score++;
+            Destroy(gameObject);
+        }
+    }
+}
